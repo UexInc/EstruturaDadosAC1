d68c8e34df700441f48e34b710cac77ae8223b23
UexInc <78887757+UexInc@users.noreply.github.com>
2021-02-11 11:16:14 -0300

    Update initial
    Inicializando o projeto

---
+++ EstruturaDadosAC1/src/arranjos/Arranjo.java
@@ -0,0 +1,27 @@
+package arranjos;
+
+public class Arranjo {
+
+	Integer[] a = {940, 880, 830, 790, 750, 660, 650, 590, 510, 940};
+	
+	// TODO: Criar o método do menor no array
+	public Integer menor() {
+		return null;
+	}
+
+	// TODO: Criar o método do maior no array
+	public Integer maior() {
+		return null;
+	}
+
+	// TODO: Criar o método da soma no array
+	public Integer soma() {
+		return null;
+	}
+
+	// TODO: Criar o método das repetições no array
+	public Integer repeticoes(int i) {
+		return null;
+	}
+
+}
